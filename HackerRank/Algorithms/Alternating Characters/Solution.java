31e41b7d0537ba4a06091db200e190ffc5516b9a
ppendre <47362160+ppendre@users.noreply.github.com>
2019-02-26 18:20:39 +0100

    Create Solution.java

---
+++ HackerRank/Algorithms/Alternating Characters/Solution.java
@@ -0,0 +1,56 @@
+/*	Problem description :  https://www.hackerrank.com/challenges/alternating-characters
+ *
+ *	Time complexity: O(n)
+ *	
+ *	Environment: www.hackerrank.com - Java 8
+ *	
+ * 	To use outside HackerRank please modify 
+ * 	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));   line to
+ 	BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+ */	
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+        int result = 0;
+        char prevChar = s.charAt(0);	// store the last correct char
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == prevChar) {
+                result++;
+            }
+            else prevChar = s.charAt(i);
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
